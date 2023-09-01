e9bc717ad8d2655bf928ecadbb328a57280225fc
Robert <robert.kondr@gmail.com>
2023-09-01 12:42:37 +0700

    правки

--- zd10.py
+++ zd10.py
@@ -7,27 +7,27 @@
 # # 2
 # # *
 
-# import random
+import random
 
-# from random import randint
+from random import randint
 
-# amount_coin = int(input('введите количество монет: '))
+amount_coin = int(input('введите количество монет: '))
 
-# m = 0
-# k = 0
-# coins = [0, 1]
-# for i in range(amount_coin):
-#     random.shuffle(coins)
-#     print(f'все монеты{coins}')
-#     if int(coins[0]) == 0:
-#         k += 1
-#     if int(coins[0]) == 1:
-#         m += 1
-# print(f'всего монет {m, k}')
+m = 0
+k = 0
+coins = [0, 1]
+for i in range(amount_coin):
+    random.shuffle(coins)
+    print(f'все монеты{coins}')
+    if int(coins[0]) == 0:
+        k += 1
+    if int(coins[0]) == 1:
+        m += 1
+print(f'всего монет {m, k}')
 
-# if m > k:
-#     ans = k
-# else:
-#     ans = m
+if m > k:
+    ans = k
+else:
+    ans = m
 
-# print(f"минимальное количество монет, которые нужно перевернуть{ans}")
+print(f"минимальное количество монет, которые нужно перевернуть{ans}")
